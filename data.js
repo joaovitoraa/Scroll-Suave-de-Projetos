de2f669eb6542bf584d2e494e9d26c20e9a3f267
João Vitor <sanvitor2013@gmail.com>
2025-02-20 10:21:26 -0300

    upload

---
+++ data.js
@@ -0,0 +1,50 @@
+export const awards = [
+  {
+    name: 'Independet Day',
+    type: 'Nominee',
+    project: 'INNOVATE 2024',
+    label: 'Awwwards',
+    imagem:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRw446cCxNCcr-fneFhg1ky-vpiuKY7oS7GEQ&s',
+  },
+  {
+    name: 'APk',
+    type: 'wwwards',
+    project: 'LVXH - AMOT',
+    label: 'See Live',
+    imagem:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYNZIIZoYyhGgdzsa4h6SlnNvHriONHDQOSg&s',
+  },
+  {
+    name: 'Site of the day',
+    type: 'wwwards',
+    project: 'LVXH - AMOT',
+    label: 'See Live',
+    imagem:
+      'https://assets.awwwards.com/awards/element/2024/11/67451c4b1699e388412779.jpg',
+  },
+  {
+    name: 'Site of the day',
+    type: 'wwwards',
+    project: 'LVXH - AMOT',
+    label: 'See Live',
+    imagem:
+      'https://assets.awwwards.com/awards/submissions/2023/04/642c54c493848842547417.jpeg',
+  },
+  {
+    name: 'Independet DAy',
+    type: 'Nominee',
+    project: 'INNOVATE 2024',
+    label: 'Awwwards',
+    imagem:
+      'https://assets.awwwards.com/awards/element/2024/11/67451c4b1699e388412779.jpg',
+  },
+  {
+    name: 'Site of the day',
+    type: 'wwwards',
+    project: 'LVXH - AMOT',
+    label: 'See Live',
+    imagem:
+      'https://assets.awwwards.com/awards/element/2024/11/67451c4b1699e388412779.jpg',
+  },
+];
